7cb63263fdadabd340e1e47b38ab18a11993c238
Cassie Heinle <cheinle@ntu.edu.sg>
2020-05-15 15:40:07 +0800

    add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+Calcultor library containing basic math operations. Part of Real Python tutorial on CI
+"""
+
+def add(first_term, second_term):
+	return first_term + second term
+
+def subtract(first_term, second_term):
+	return first_term - second_term
+
